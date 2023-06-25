39e2dfe3c40615a08622d46688d4f7c5456063f0
Dip Singha <dipsingha2@gmail.com>
2023-06-26 01:18:33 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="font-medium text-5xl text-teal-500">
+          List of Github Users
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
