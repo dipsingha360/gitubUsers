10df9ec521883d3afbf925472378869be08d7516
Dip Singha <dipsingha2@gmail.com>
2023-06-28 13:36:59 +0600

    load data in the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,68 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import loading from "./loading";
 
 function Card() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch("https://api.github.com/users");
+      setUsers(await response.json());
+    } catch (error) {
+      console.log("err" + error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // if (loading) {
+  //   return <loading />;
+  // }
+
   return (
     <div>
       <h2 className="font-medium text-5xl text-blue-50 text-center pt-10 pb-20">
         List of Github Users
       </h2>
       <div className="all-cards mx-10 flex gap-6 flex-row flex-wrap">
-        <div className="cards flex justify-around items-center py-20 bg-slate-300 shadow-sm w-[24%] h-40 rounded-lg">
-          <div className="card-left img ">
-            <img
-              className="w-32 h-32 object-cover bg-slate-500 rounded-lg "
-              src="https://mir-s3-cdn-cf.behance.net/user/276/f4374748121033.62f37e8f85ed9.png"
-              alt=""
-            />
-          </div>
-          <div className="card-right infos flex flex-col gap-2">
-            <h4 className="text-2xl font-bold  text-[#2b2b2b]">Dip Singha</h4>
-            <p className="font-normal  text-[#696666]">Web Developer</p>
-            <div className="info flex gap-8 text-center bg-white rounded-md px-4">
-              <div className="article">
-                <p className="font-normal text-[#817f7f] ">Article</p>
-                <p className="font-medium text-[#2b2b2b] ">38</p>
+        {users.map((user, index) => {
+          return (
+            <>
+              <div className="cards flex justify-around items-center py-20 bg-slate-300 shadow-sm w-[24%] h-40 rounded-lg">
+                <div className="card-left img ">
+                  <img
+                    className="w-32 h-32 object-cover bg-slate-500 rounded-lg "
+                    src={user.avatar_url}
+                    alt=""
+                  />
+                </div>
+                <div className="card-right infos flex flex-col gap-2">
+                  <h4 className="text-2xl font-bold  text-[#2b2b2b]">
+                    {user.login}
+                  </h4>
+                  <p className="font-normal  text-[#696666]">Developer</p>
+                  <div className="info flex gap-8 text-center bg-white rounded-md px-4">
+                    <div className="article">
+                      <p className="font-normal text-[#817f7f] ">Article</p>
+                      <p className="font-medium text-[#2b2b2b] ">38</p>
+                    </div>
+                    <div className="followers">
+                      <p className="font-normal text-[#817f7f] ">Type</p>
+                      <p className="font-medium text-[#2b2b2b] ">{user.type}</p>
+                    </div>
+                    <div className="rating">
+                      <p className="font-normal text-[#817f7f] ">Rating</p>
+                      <p className="font-medium text-[#2b2b2b] ">4.5</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="followers">
-                <p className="font-normal text-[#817f7f] ">Followers</p>
-                <p className="font-medium text-[#2b2b2b] ">880</p>
-              </div>
-              <div className="rating">
-                <p className="font-normal text-[#817f7f] ">Rating</p>
-                <p className="font-medium text-[#2b2b2b] ">4.5</p>
-              </div>
-            </div>
-          </div>
-        </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
